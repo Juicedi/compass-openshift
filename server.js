4dcaedcc8edf096cee94989c9402bd334d7eaf80
Juicedi <jussi.latvaniemi@gmail.com>
2016-04-27 09:59:36 +0300

    mongodatabases

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 #!/bin/env node
-//  OpenShift sample Node application
+ //  OpenShift sample Node application
 var express = require('express');
-var fs      = require('fs');
+var fs = require('fs');
 
 
 /**
  *  Define the sample application.
  */
-var SampleApp = function() {
+var SampleApp = function () {
 
     //  Scope.
     var self = this;
@@ -20,10 +20,10 @@ var SampleApp = function() {
     /**
      *  Set up server IP address and port # using env variables/defaults.
      */
-    self.setupVariables = function() {
+    self.setupVariables = function () {
         //  Set the environment variables we need.
         self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+        self.port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
         if (typeof self.ipaddress === "undefined") {
             //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
@@ -37,9 +37,11 @@ var SampleApp = function() {
     /**
      *  Populate the cache.
      */
-    self.populateCache = function() {
+    self.populateCache = function () {
         if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
+            self.zcache = {
+                'index.html': ''
+            };
         }
 
         //  Local cache for static content.
@@ -51,7 +53,9 @@ var SampleApp = function() {
      *  Retrieve entry (content) from cache.
      *  @param {string} key  Key identifying content to retrieve from cache.
      */
-    self.cache_get = function(key) { return self.zcache[key]; };
+    self.cache_get = function (key) {
+        return self.zcache[key];
+    };
 
 
     /**
@@ -59,30 +63,57 @@ var SampleApp = function() {
      *  Terminate server on receipt of the specified signal.
      *  @param {string} sig  Signal to terminate on.
      */
-    self.terminator = function(sig){
+    self.terminator = function (sig) {
         if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
+            console.log('%s: Received %s - terminating sample app ...',
+                Date(Date.now()), sig);
+            process.exit(1);
         }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
+        console.log('%s: Node server stopped.', Date(Date.now()));
     };
 
 
     /**
      *  Setup termination handlers (for exit and a list of signals).
      */
-    self.setupTerminationHandlers = function(){
+    self.setupTerminationHandlers = function () {
         //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
+        process.on('exit', function () {
+            self.terminator();
+        });
 
         // Removed 'SIGPIPE' from the list - bugz 852598.
         ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
          'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
+        ].forEach(function (element, index, array) {
+            process.on(element, function () {
+                self.terminator(element);
+            });
         });
     };
+
+    // Connect to the db
+    var connection_string = '127.0.0.1:27017/nodejs';
+
+    if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+        connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+            process.env.OPENSHIFT_APP_NAME;
+    }
+
+    var mongojs = require('mongojs');
+    var db = mongojs(connection_string, ['books']);
+    var books = db.collection('books');
+    // similar syntax as the Mongo command-line interface
+    // log each of the first ten docs in the collection
+    db.books.find({}).limit(10).forEach(function (err, doc) {
+        if (err) throw err;
+        if (doc) {
+            console.dir(doc);
+        }
+    });
 
 
     /*  ================================================================  */
@@ -92,15 +123,15 @@ var SampleApp = function() {
     /**
      *  Create the routing table entries + handlers for the application.
      */
-    self.createRoutes = function() {
-        self.routes = { };
+    self.createRoutes = function () {
+        self.routes = {};
 
-        self.routes['/asciimo'] = function(req, res) {
+        self.routes['/asciimo'] = function (req, res) {
             var link = "http://i.imgur.com/kmbjB.png";
             res.send("<html><body><img src='" + link + "'></body></html>");
         };
-        
-        self.routes['/getFavourite'] = function(req, res) {
+
+        self.routes['/getFavourite'] = function (req, res) {
             var favourite = {
                 lat: 12.1212,
                 lng: 23.2323
@@ -108,9 +139,9 @@ var SampleApp = function() {
             res.send(favourite);
         };
 
-        self.routes['/'] = function(req, res) {
+        self.routes['/'] = function (req, res) {
             res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
+            res.send(self.cache_get('index.html'));
         };
     };
 
@@ -119,7 +150,7 @@ var SampleApp = function() {
      *  Initialize the server (express) and create the routes and register
      *  the handlers.
      */
-    self.initializeServer = function() {
+    self.initializeServer = function () {
         self.createRoutes();
         self.app = express.createServer();
 
@@ -133,7 +164,7 @@ var SampleApp = function() {
     /**
      *  Initializes the sample application.
      */
-    self.initialize = function() {
+    self.initialize = function () {
         self.setupVariables();
         self.populateCache();
         self.setupTerminationHandlers();
@@ -146,15 +177,15 @@ var SampleApp = function() {
     /**
      *  Start the server (starts up the sample application).
      */
-    self.start = function() {
+    self.start = function () {
         //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
+        self.app.listen(self.port, self.ipaddress, function () {
             console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
+                Date(Date.now()), self.ipaddress, self.port);
         });
     };
 
-};   /*  Sample Application.  */
+}; /*  Sample Application.  */
 
 
 
@@ -164,4 +195,3 @@ var SampleApp = function() {
 var zapp = new SampleApp();
 zapp.initialize();
 zapp.start();
-
