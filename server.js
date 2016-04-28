ff2f6dee138a55d5e655a3f457b40df8b4cc26fc
Juicedi <jussi.latvaniemi@gmail.com>
2016-04-28 15:22:46 +0300

    database parameter

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ var SampleApp = function () {
             // the client db connection scope is wrapped in a callback:
             MongoClient.connect('mongodb://' + connection_string, function (err, db) {
                 if (err) throw err;
-                var collection = db.collection('books').find().limit(req.query.number).toArray(function (err, docs) {
+                var collection = db.collection('books').find().limit(2).toArray(function (err, docs) {
                     console.dir(docs);
                     res.send(docs);
                     db.close();
